ed7cbf77788496227ad64a769e9f7f5669d9b5ac
JianFGO <JianLin.L.He@student.shu.ac.uk>
2023-11-24 09:46:56 +0000

    fbxds

--- clock.js
+++ clock.js
@@ -1,3 +1,7 @@
 (function(){ 
 console.info("Hello JS"); 
+const myNode = document.createElement("div");
+const myNode = document.getElementById("digitalClock");
+digitalClock.appendChild(myNode)
+
 })();
