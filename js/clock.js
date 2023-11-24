cdd99130c6beb1b8dd1853357dfb9920348ce986
JianFGO <JianLin.L.He@student.shu.ac.uk>
2023-11-24 10:53:36 +0000

    sdgf

--- js/clock.js
+++ js/clock.js
@@ -8,21 +8,25 @@ const myDate = new Date();
 console.dir(myDate);
 myNode.innerHTML = myDate.toDateString();
 
-// show only hours, minutes and seconds
-myNode.innerHTML = myDate.toLocaleTimeString([],
-     { hour: "2-digit", minute: "2-digit", second: "2-digit" });
+  const secondHand = document.getElementById("secondHand");
+  const minHand = document.getElementById("minHand");
+  const hrHand = document.getElementById("hrHand");
+  hrHand.style.backgroundColor = '#0f0';
+  
+const updateTime = () => {
+    console.info("tick tock");
+    const myDate = new Date();
+    myNode.innerHTML = myDate.toLocaleTimeString([], 
+        { hour: "2-digit", minute: "2-digit", second: "2-digit" });   
+};
+setInterval(updateTime, 1000);
+updateTime();
+
+
   const updateClockHands = (myDate) => {
     const seconds = myDate.getSeconds();
     const rotSeconds = seconds * 6 - 90;
     secondHand.style.transform = `rotate(${rotSeconds}deg)`;
   };
 
-  const updateTime = () => {
-    console.info("tick tock");
-    const myDate = new Date();
-    myNode.innerHTML = myDate.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", second: "2-digit" });
-    updateClockHands(myDate);
-
-
-  };
 })();
